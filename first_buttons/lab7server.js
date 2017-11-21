ce7a8748da6c37826217fa06b754a158d304bfff
scvstackkk <scv.stack@gmail.com>
2017-11-21 00:44:40 -0600

    Finished modifiying lab7server.js to query fluto006.till_buttons for the button objects.

--- first_buttons/lab7server.js
+++ first_buttons/lab7server.js
@@ -1,12 +1,38 @@
-var express=require('express'),
-app = express(),
-port = process.env.PORT || 1337;
+var credentials = require("../node_examples/credentials.json");
+var mysql = require("mysql");
+var Promise = require('bluebird');
+var using = Promise.using;
+Promise.promisifyAll(require("mysql/lib/Connection").prototype);
+Promise.promisifyAll(require("mysql/lib/Pool").prototype);
 
-var buttons=[{"buttonID":1,"left":10,"top":70,"width":100,"label":"hotdogs","invID":1},{"buttonID":2,"left":110,"top":70,"width":100,"label":"hambugers","invID":2},{"buttonID":3,"left":210,"top":70,"width":100,"label":"bannanas","invID":3},{"buttonID":4,"left":10,"top":120,"width":100,"label":"milkduds","invID":4}]; //static buttons
+var pool = mysql.createPool(credentials); // Setup the connection pool using our credentials.
 
-app.use(express.static(__dirname + '/public')); //Serves the web pages
-app.get("/buttons",function(req,res){ // handles the /buttons API
-  res.send(buttons);
-});
+var getConnection=function(){
+  return pool.getConnectionAsync().disposer(
+    function(connection){return connection.release();}
+  );
+};
 
-app.listen(port);
+var query=function(command){
+  return using(getConnection(), function(connection){
+    return connection.queryAsync(command);
+  });
+};
+
+sql="select * from fluto006.till_buttons";
+
+var result=query(mysql.format(sql)); //result is a promise
+result.then(function(dbfs,err){
+  var express=require('express'),
+  app = express(),
+  port = process.env.PORT || 1337;
+
+  var buttons = JSON.stringify(dbfs);
+
+  app.use(express.static(__dirname + '/public')); //Serves the web pages
+  app.get("/buttons",function(req,res){ // handles the /buttons API
+    res.send(buttons);
+  });
+
+  app.listen(port);
+}).then(function(){pool.end()});
