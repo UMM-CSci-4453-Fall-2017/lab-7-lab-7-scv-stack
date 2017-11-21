6fd556090e416c85b31eb5008623ebb06f880194
scvstackkk <scv.stack@gmail.com>
2017-11-21 01:01:46 -0600

    Re-wrote lab7server to use callbacks to app.get for the sql query. Made use of lab 8's material to help with this.

--- first_buttons/lab7server.js
+++ first_buttons/lab7server.js
@@ -1,38 +1,23 @@
-var credentials = require("../node_examples/credentials.json");
-var mysql = require("mysql");
-var Promise = require('bluebird');
-var using = Promise.using;
-Promise.promisifyAll(require("mysql/lib/Connection").prototype);
-Promise.promisifyAll(require("mysql/lib/Pool").prototype);
 
-var pool = mysql.createPool(credentials); // Setup the connection pool using our credentials.
+var express=require('express'),
+mysql = require('mysql'),
+credentials = require('../node_examples/credentials.json'),
+app = express(),
+port = process.env.PORT || 1337;
 
-var getConnection=function(){
-  return pool.getConnectionAsync().disposer(
-    function(connection){return connection.release();}
-  );
-};
+var connection = mysql.createConnection(credentials);
+var buttons;
+app.use(express.static(__dirname + '/public')); //Serves the web pages
+app.get("/buttons",function(req,res){ // handles the /buttons API
+  var sql = 'SELECT * FROM fluto006.till_buttons';
+  connection.query(sql, (function(res){
+    return function(err, rows, fields){
+      if(err){
+        console.log(err);
+      } else {
+        res.send(rows);
+      }
+  }})(res));
+});
 
-var query=function(command){
-  return using(getConnection(), function(connection){
-    return connection.queryAsync(command);
-  });
-};
-
-sql="select * from fluto006.till_buttons";
-
-var result=query(mysql.format(sql)); //result is a promise
-result.then(function(dbfs,err){
-  var express=require('express'),
-  app = express(),
-  port = process.env.PORT || 1337;
-
-  var buttons = JSON.stringify(dbfs);
-
-  app.use(express.static(__dirname + '/public')); //Serves the web pages
-  app.get("/buttons",function(req,res){ // handles the /buttons API
-    res.send(buttons);
-  });
-
-  app.listen(port);
-}).then(function(){pool.end()});
+app.listen(port);
